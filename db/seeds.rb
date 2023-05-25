a279bc1ca65ddd190b243b5f1eb08907faaa9516
Maxime Moreira <maxime.moreira@gmail.com>
2023-05-25 12:02:28 +0200

    added seeding method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# cleaning the DB
+Movie.destroy_all
+
+puts "#{Movie.all.count} film in the db"
+
+puts "creating the DB"
+
+15.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: (rand()*10).round(1)
+  )
+end
+
+puts "#{Movie.all.count} film in the db"
